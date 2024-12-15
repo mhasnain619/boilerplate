36a684ac792b3e5e5b1cd265ffdd0df00c5ebf18
Muhammad Hasnain <hasnain0893@gmail.com>
2024-12-15 15:57:16 +0500

    boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
       <div className='backGr'>
         <h1>Hello React</h1>
         <p>React is a JavaScript library for building user interfaces.</p>
+        <p>It is maintained by Facebook and a community of individual developers and companies.</p>
+        <p>React can be used for developing web and mobile applications.</p>
+
 
       </div>
     </>
