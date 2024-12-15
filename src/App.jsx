0989b44c7ffb8452274ade8faf7ba526c8879d1f
Muhammad Hasnain <hasnain0893@gmail.com>
2024-12-15 15:56:11 +0500

    boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='backGr'>
+        <h1>Hello React</h1>
+        <p>React is a JavaScript library for building user interfaces.</p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
